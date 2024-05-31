4d05fc0bb54fb56ff3be9ae7ac8d07a1dc934e8f
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-05-31 20:06:09 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "primary-dark-blue": "#121826",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
