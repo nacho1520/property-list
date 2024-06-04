f7ade897b6905b7c83ba0db3a3a147e84af44ff9
Ignacio Lopez <ignaciolopestela12@gmail.com>
2024-06-04 12:52:44 -0300

    change toggle color

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -3,9 +3,9 @@ const Toggle = ({ label, active, setActive }) => {
         <div className="inline-flex flex-wrap gap-2 items-center">
             <div 
                 onClick={ setActive }
-                className="w-12 h-6 rounded-[20px] bg-toggle-blue flex justify-around items-center cursor-pointer"
+                className={`w-12 h-6 rounded-[20px] flex justify-around items-center cursor-pointer ${ !active ? 'bg-toggle-blue' : 'bg-primary-dark-blue' }`}
             >
-                <div className={`rounded-full w-5 h-5 ${ active ? 'bg-toggle-blue' : 'bg-white-font' } `}></div>
+                <div className={`rounded-full w-5 h-5 ${ active ? 'bg-primary-dark-blue' : 'bg-white-font' } `}></div>
                 <div className={`rounded-full w-5 h-5 ${ active ? 'bg-white-font' : 'bg-toggle-blue' } `}></div>
             </div>
             <p className="text-xs text-white-font font-medium">{ label }</p>
